a56912bf2188cf2cd87f59ac4baafd347bc98116
seshadri-05 <seshadri30@gmail.com>
2025-09-11 12:07:33 +0530

    committed

--- 11_range/range.go
+++ 11_range/range.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// nums := []int{6, 7, 8}
+
+	// sum := 0
+
+	//for i, num := range nums {
+	// sum = sum + num
+	//	fmt.Println(num, i)
+	//}
+
+	/* m := map[string]string{"fname": "john", "lname": "doe"}
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	} */
+
+	// unicode code
+	for i, c := range "Golang" {
+		fmt.Println(i, string(c))
+	}
 }
---
+++ 12_function/functions.go
@@ -0,0 +1,35 @@
+package main
+
+/* func add(a int, b int) int {
+	return a + b
+} */
+/*
+func getLanguages() (string, string, string) {
+	return "golang", "javascript", "c"
+}
+*/
+
+// func processIt(fn func(a int) int) {
+//	fn(1)
+// }
+
+func processIt() func(a int) int {
+	return func(a int) int {
+		return 4
+	}
+}
+
+func main() {
+
+	/* result := add(5, 3)
+	fmt.Println(result) */
+
+	/* fn := func(a int) int {
+		return 2
+	}
+	processIt(fn) */
+
+	fn := processIt()
+	fn(6)
+
+}
